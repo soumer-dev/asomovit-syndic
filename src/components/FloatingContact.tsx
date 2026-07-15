ff208e90b9689d3202fb470f6816e1fa63927356
soumer-dev <nezha@monarkit.net>
2026-07-15 15:32:41 +0100

    Adjust FloatingContact position for improved visibility

--- src/components/FloatingContact.tsx
+++ src/components/FloatingContact.tsx
@@ -26,7 +26,7 @@ const btnBase =
 
 export function FloatingContact() {
   return (
-    <div className="fixed bottom-4 right-4 z-40 flex flex-col gap-2.5 sm:bottom-5 sm:right-5">
+    <div className="fixed bottom-20 right-4 z-40 flex flex-col gap-2.5 sm:right-5">
       {/* WhatsApp */}
       <a
         href={WHATSAPP_URL}
